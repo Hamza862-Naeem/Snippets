50cd22dc5915ee509f32641463787f94d50c24d2
Hamza862-Naeem <h.mhamzanaeem862@gmail.com>
2023-09-18 21:55:40 +0500

    first commit

---
+++ if.dart
@@ -0,0 +1,87 @@
+import'dart:io';
+
+//is rainig or not
+/* bool isRaining = true;
+void main() {
+  // is it raining?
+  
+
+if(isRaining){
+  print("Dont forget to bring umbrella");
+}
+else{
+  print("Enjoy the sunny day");
+}
+}*/
+//even odd
+/* void main() {
+  int number= 4;
+  if(number % 2==0){
+    print("Even");
+  }
+  else{
+   print ('odd');
+  }
+}*/
+/*bool isLoggedIn = true;
+void main(){
+  if(isLoggedIn){
+    print("welcome new user");
+  }
+else{
+  print("not logged in");
+}
+}*/
+
+//Is the user's age greater than 18?
+
+/*void main(){
+  print("Enter age");
+  int age= int.parse(stdin.readLineSync()!);
+  if (age>18){
+    print("User is adult");
+  }
+  else{
+    print("user is child");
+  }
+}*/
+
+//is Temperature below freezing
+/*void main(){
+  print("enter temperature");
+  int temperature = int.parse(stdin.readLineSync()!);
+  if (temperature<=0){
+    print('Freezing');
+  }
+  else{
+    print("The weather is fine");
+  }
+}*/
+//Is a number positive, negative, or zero?
+/*void main(){
+  stdout.write("Enter number:");
+  int num = int.parse(stdin.readLineSync()!);
+  if(num>0){
+    print("It is positive");
+  }
+  else if (num<0){
+    print('It is negative');
+  }
+  else{
+    print('It is zero');
+  }
+}*/
+
+//Is it a weekend or a weekday?
+/*void main(){
+  stdout.write('Enter Day\n');
+  String? Day= stdin.readLineSync();
+  if(Day=='Saturday'|| Day=='Sunday' ){
+    print('it is weekend');
+  }
+  else{
+    print('It is a weekday');
+  }
+}*/
+
+
