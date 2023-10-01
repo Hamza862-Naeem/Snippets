eff81add220176c0502c89609196df2959e78fac
mahamzanaeem862@gmail.com <mhamzanaeem862@gmail.com>
2023-10-01 20:31:26 +0500

    1st oct

--- else.dart
+++ else.dart
@@ -1,1 +1,28 @@
+// ignore: unused_import
 import'dart:io';
+
+
+
+/*void main(){
+  String someValue = "Hi!";
+
+if(someValue!="Hi"){
+  print("No Hi");
+}
+else{
+  print("Hi");
+}
+}*/
+
+/*void main(){
+String somevalue= "yeah";
+//if(somevalue.isEmpty)
+ //if(somevalue.startsWith('h'))
+ {
+print("No yeah");}
+else{
+  print("Yup");
+}}*/
+
+
+
